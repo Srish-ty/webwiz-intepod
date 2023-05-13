9a6d4810bf51d868c5799eadf678822f5a0c0d75
adityaarunav <122117437+adityaarunav@users.noreply.github.com>
2023-05-13 19:24:44 +0530

    feat : added glasseffect

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ hamburger.addEventListener("click",() => {
     navMenu.classList.toggle("active");
 
 })
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=> {
+console.log(Array.from(document.querySelectorAll(".nav-item")));
+(Array.from(document.querySelectorAll(".nav-item"))).forEach(n => n.addEventListener("click",()=> {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
