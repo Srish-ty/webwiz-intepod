70945b2d145f1c018f477bc71d9b8b44d2ded565
Srishty M <srishtymangutte@gmail.com>
2023-05-14 06:25:30 +0000

    feat: added dark-light mode

---
+++ darkMode.js
@@ -0,0 +1,19 @@
+var dm= document.getElementsByClassName('dark-mode')[0];
+var boole=0;
+var sec = document.getElementsByClassName('sec');
+dm.addEventListener('click',changeMode);
+
+function changeMode(){
+    if(boole==0){
+        document.body.style.backgroundColor='azure';
+        document.body.style.color='night';
+        for(s of sec){s.style.color='rgb(13, 12, 28)';}
+        boole=1;
+    }
+    else{
+        document.body.style.backgroundColor='rgb(13, 12, 28)';
+        document.body.style.color='white';
+        for(s of sec){s.style.color='white';}
+        boole=0;
+    }
+}
